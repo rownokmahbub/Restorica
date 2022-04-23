191e21ac26b86f935dca90074cb4f9f39d6b7005
rownokmahbub <53168870+rownokmahbub@users.noreply.github.com>
2022-04-23 22:06:36 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      
+      fontFamily: {
+      'Dancing': ['Dancing Script'],
+      'josefin': ['Josefin Sans'],
+
+    },
+
+
+      boxShadow: {
+        'cla-blue': '35px 35px 68px 0px rgba(224, 230, 249, 0.5), inset -12px -12px 16px 0px rgba(224, 230, 249, 0.6), inset 0px 11px 28px 0px rgb(255, 255, 255)',
+        'cla-green': '35px 35px 68px 0px rgba(224, 249, 239, 0.5), inset -12px -12px 16px 0px rgba(224, 249, 239, 0.6), inset 0px 11px 28px 0px rgb(255, 255, 255)',
+        'cla-violate': '35px 35px 68px 0px rgba(244, 224, 249, 0.5), inset -12px -12px 16px 0px rgba(244, 224, 249, 0.6), inset 0px 11px 28px 0px rgb(255, 255, 255)',
+        'cla-slate': '35px 35px 68px 0px rgba(241, 245, 249, 0.5), inset -12px -12px 16px 0px rgba(241, 245, 249, 0.6), inset 0px 11px 28px 0px rgb(255, 255, 255)',
+        'cla-gray': '35px 35px 68px 0px rgba(249, 250, 251, 0.5), inset -12px -12px 16px 0px rgba(249, 250, 251, 0.6), inset 0px 11px 28px 0px rgb(255, 255, 255)',
+        'cla-red': '35px 35px 68px 0px rgba(254, 242, 242, 0.5), inset -12px -12px 16px 0px rgba(254, 242, 242, 0.6), inset 0px 11px 28px 0px rgb(255, 255, 255)',
+        'cla-cyan': '35px 35px 68px 0px rgba(236, 254, 255, 0.5), inset -12px -12px 16px 0px rgba(236, 254, 255, 0.6), inset 0px 11px 28px 0px rgb(255, 255, 255)',
+        'cla-fuchsia': '35px 35px 68px 0px rgba(253, 244, 255, 0.5), inset -12px -12px 16px 0px rgba(253, 244, 255, 0.6), inset 0px 11px 28px 0px rgb(255, 255, 255)',
+        'cla-pink': '35px 35px 68px 0px rgba(253, 242, 248, 0.5), inset -12px -12px 16px 0px rgba(253, 242, 248, 0.6), inset 0px 11px 28px 0px rgb(255, 255, 255)',
+        'neo': '20px 20px 60px #bebebe, -20px -20px 60px #ffffff',
+      }
+    },
+  },
+  plugins: [],
+}
